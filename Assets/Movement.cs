370d6ab59057cf85ee52d63dd107fd503f2b7b10
Broden D'Angio <Brodendangio@gmail.com>
2021-10-02 15:57:31 -0700

    Movement Basics Done

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -4,66 +4,121 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-
     public Rigidbody2D rb;
-    private float dirX, dirY, moveSpeed;
-    private float gravityMin = 0, gravityMax = 12, currentGravity = 0;
+    public  float dirX, dirY;
+    public float jumpForce, moveSpeed;
+    public float climbingGravity, defaultGravity, currentGravity;
 
+    public bool facingRight = true;
 
+    public bool isGrounded;
+    public Transform groundCheck;
+    public float checkRadius;
+    public LayerMask whatIsGround;
+
+    public bool isTouchingFront;
+    public Transform frontCheck;
+    public LayerMask whatIsBuilding;
+
+    public bool isClimbing;
+
+    public bool isJumping;
+
+    public bool wallSliding;
+    public float wallSlidingSpeed;
+   
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        moveSpeed = 5f;
+        currentGravity = defaultGravity;
     }
 
     // Update is called once per frame
     void Update()
-    {
+    {    
 
-        dirX = Input.GetAxisRaw("Horizontal") * moveSpeed;
-        dirY = Input.GetAxisRaw("Vertical") * moveSpeed;
+        //Check if Grounded and Touching (using a "checkRadious" ~)
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, whatIsGround);
+        isJumping = isGrounded ? false : true;
 
-        if (Input.GetKeyDown(KeyCode.Space))
+        //Jump if grounded, but not touching building side
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded && !isJumping && !isClimbing)
+            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y + jumpForce);
+
+
+        //Movement Horizontal - Locked if jumping
+        if (!isJumping)
         {
-            SwitchGravity();
+            float input = Input.GetAxisRaw("Horizontal");
+            rb.velocity = new Vector2(input * moveSpeed, rb.velocity.y);
+
+            //Flip if pressing left or right
+            if (input > 0 && !facingRight && !isClimbing)
+                Flip();
+            else if (input < 0 && facingRight && !isClimbing)
+                Flip();
         }
 
 
-    }
-
-    void StartClimb()
-    {
-        
-    }
-
-    void SwitchGravity()
-    {
-        Debug.Log("RAn");
-
-        currentGravity = currentGravity == gravityMin ? gravityMax : gravityMin;
-        rb.gravityScale = currentGravity;
-        /*
-        if(currentGravity == gravityMin)
-       
-            currentGravity = gravityMin;
+        //Check for building and climb if possible
+        isTouchingFront = Physics2D.OverlapCircle(frontCheck.position, checkRadius, whatIsBuilding);
+        if (Input.GetKeyDown(KeyCode.W) && isGrounded && isTouchingFront)
+        {
+            Collider2D[] buildingBricks = Physics2D.OverlapCircleAll(frontCheck.position, checkRadius, whatIsBuilding);
+            for (int i = 0; i < buildingBricks.Length; i++)
+            {
+                if (transform.localScale.x != buildingBricks[i].transform.localScale.x)
+                {
+                    transform.position = new Vector2(buildingBricks[i].transform.position.x + buildingBricks[i].transform.localScale.x, transform.position.y);
+                    isClimbing = true;
+                    break;
+                }
+            }
         }
-             rb.gravityScale  = gravityMax; //to substract
 
-        else if (currentGravity == gravityMax)
-            rb.gravityScale = gravityMin; //to substract
-
-    */
-    }
-
-    void Jump()
-    {
        
+        if (isClimbing)
+        {
+            currentGravity = climbingGravity;
+            float moveX = 0;
+            float moveY = 0;
+            moveY = Input.GetAxisRaw("Vertical") * (moveSpeed / 2);
+
+            if (isTouchingFront)
+                moveY = Input.GetAxisRaw("Vertical") * (moveSpeed / 2);
+
+            if (!isTouchingFront)
+                moveY = Mathf.Clamp(Input.GetAxisRaw("Vertical") * (moveSpeed / 2), float.MinValue, 0f);
+
+            rb.velocity = new Vector2(moveX, moveY);
+        }
+
+        if (isClimbing && Input.GetKeyDown(KeyCode.Space))
+        {
+            isClimbing = !isClimbing;
+            rb.velocity = new Vector2(-transform.localScale.x * 1.5f, rb.velocity.y + jumpForce / 4);
+            isJumping = true;
+        }
+
+        if (isClimbing && (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.D)) && isGrounded)
+            isClimbing = !isClimbing;
+
+        //Set Gravity
+        currentGravity = isClimbing ? climbingGravity : defaultGravity;
+        rb.gravityScale = currentGravity;  
     }
 
-
-    public void FixedUpdate()
+    void Flip()
     {
-        rb.velocity = new Vector2(dirX, dirY);
+        transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
+        facingRight = !facingRight;
     }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawSphere(frontCheck.position, checkRadius);
+    }
+
 }
